b39630e7ab44d85532462ae318d228053fc825a6
Han56 <2279627931@qq.com>
2021-11-03 20:35:05 +0800

    双指针模块建立

--- doublePointer/src/test/Merge.java
+++ doublePointer/src/test/Merge.java
@@ -1,6 +1,14 @@
-package test;/**
-*@description 功能描述
-*@author han56
-*@create 2021/11/2 下午8:01
-*/public class Merge {
+package test;
+
+/**
+ * @author han56
+ * @description 功能描述
+ * @create 2021/11/2 下午8:01
+ */
+public class Merge {
+
+    public void test(){
+
+    }
+
 }
